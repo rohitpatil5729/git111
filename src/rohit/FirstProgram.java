d72b9b7d4eb92b0c52ec34f10e1e6765af4916cc
[200~rohitpatil5729~ <rohitpatil5729@gmail.com>
2024-12-04 21:27:32 +0530

    classpathproject

---
+++ src/rohit/FirstProgram.java
@@ -0,0 +1,27 @@
+package rohit;
+ 
+public class FirstProgram {
+	
+	
+	public static void main(String[] args) {
+		int a;
+		int b;
+		String c="Rohit";
+		String d="Rohit1";
+		a=10;
+		b=03;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a/b);
+		System.out.println();
+		System.out.println(a);	
+		
+		FirstProgram abc=new FirstProgram();
+		System.out.println(abc.m);
+		System.out.println(n);
+		}
+	
+	int m=10;//non static variable
+	static byte n=12;//Static variable
+
+}
---
+++ src/rohit/Initilization_ChangeValue.java
@@ -0,0 +1,23 @@
+package rohit;
+
+//import java.lang.classfile.attribute.ModuleMainClassAttribute;
+
+public class Initilization_ChangeValue {
+
+	
+	public static void main(String[] args) {
+		
+		int a;
+		a=10;//initilization
+		a=25;//change value
+		System.out.println(a);
+		a=35;//change value
+		System.out.println(a);
+	
+		
+	}
+	
+	
+	
+	
+}
